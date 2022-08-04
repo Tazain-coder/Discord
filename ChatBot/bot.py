f6cdbd2d28655f82d5fa98372671b41c6847600a
Tazain-coder <59202738+Tazain-coder@users.noreply.github.com>
2022-08-04 21:13:43 +0600

    bot start

---
+++ ChatBot/bot.py
@@ -0,0 +1,21 @@
+import os
+import discord
+
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("We have logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith("$hello"):
+        await message.channel.send("hello")
+        
+client.run(BOT_TOKEN)
